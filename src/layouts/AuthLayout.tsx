a391981994c4f8e2cb8b1dc8c587e23b5e9f4734
David Barbosa <webmaster@etraining.co>
2024-03-06 08:36:49 -0500

    Updated files

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,32 +1,40 @@
-
-import { Navigate, Outlet } from 'react-router-dom';
-import { useAuthStore } from '../stores';
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuthStore } from "@stores/index";
 
 export const AuthLayout = () => {
+  const authStatus = useAuthStore((state) => state.status);
+  const checkAuthStatus = useAuthStore((state) => state.checkAuthStatus);
 
-
-  const authStatus = useAuthStore( state => state.status );
-  const checkAuthStatus = useAuthStore( state => state.checkAuthStatus );
-
-  if ( authStatus === 'pending' ) {
+  if (authStatus === "pending") {
     checkAuthStatus();
     return <>Loading...</>;
   }
 
-  if ( authStatus === 'authorized' ) {
-    return <Navigate to='/dashboard' />;
+  if (authStatus === "authorized") {
+    return <Navigate to="/dashboard" />;
   }
 
-
   return (
-    <div className="bg-gray-100 flex justify-center items-center h-screen">
-      <div className="w-1/2 h-screen hidden lg:flex lg:flex-col items-center justify-center bg-indigo-700">
-        <span className="text-white font-bold text-9xl">Zustand</span>
-        {/* <img src="https://placehold.co/1440/667fff/ffffff.png?text=Zustand&font=Montserrat"
-          alt="Placeholder Image"
-          className="object-cover w-full h-full" /> */}
+    <div className="h-screen flex">
+      <div
+        class="hidden lg:flex w-full lg:w-1/2 login_img_section
+        justify-around items-center"
+      >
+        <div class=" bg-black opacity-20 inset-0 z-0"></div>
+        <div class="w-full mx-auto px-20 flex-col items-center space-y-6">
+          <h1 class="text-white font-bold text-4xl font-sans">Simple App</h1>
+          <p class="text-white mt-1">The simplest app to use</p>
+          <div class="flex justify-center lg:justify-start mt-6">
+            <a
+              href="#"
+              class="hover:bg-indigo-700 hover:text-white hover:-translate-y-1 transition-all duration-500 bg-white text-indigo-800 mt-4 px-4 py-2 rounded-2xl font-bold mb-2"
+            >
+              Get Started
+            </a>
+          </div>
+        </div>
       </div>
-      <div className="lg:p-36 md:p-52 sm:20 p-8 w-full lg:w-1/2">
+      <div className="flex flex-col w-full lg:w-1/2 justify-center items-center bg-white space-y-8">
         <Outlet />
       </div>
     </div>
--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,6 +1,1 @@
-
-export * from './auth/auth.store';
-
-// export * from './bears/bears.store';
-// export * from './person/person.store';
-// export * from './tasks/task.store';
+export * from "./auth/auth.store";
